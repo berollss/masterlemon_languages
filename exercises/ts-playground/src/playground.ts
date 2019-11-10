5f774dc61a1e5cabed66b219a8cf9bd73e4c483c
Berta Lazaro <lazarorb@roche.com>
2019-11-10 13:40:22 +0100

    fixed slotmachine

--- exercises/ts-playground/src/playground.ts
+++ exercises/ts-playground/src/playground.ts
@@ -1,8 +1,8 @@
 /*
-* @author: Berta Lázaro Ribé
-*
-* LANGUAGE MODULE EXERCISES
-*/
+ * @author: Berta Lázaro Ribé
+ *
+ * LANGUAGE MODULE EXERCISES
+ */
 
 // 1. Array operations
 const array1 = [1, 2, 3, 4, 5];
@@ -29,22 +29,22 @@ console.log(`Result of last: ${last(array1)} - original array: ${array1}`);
 /* Concat */
 const concat = (arrayA: any[], arrayB: any[]) => [...arrayA, ...arrayB];
 console.log(
- `Result of concat: ${concat(
-   array1,
-   array2
- )} - original arrays: ${array1} -- ${array2}`
+  `Result of concat: ${concat(
+    array1,
+    array2
+  )} - original arrays: ${array1} -- ${array2}`
 );
 
 /* Multiple concat */
 const array3 = [true, false];
 const concatMultiple = (...arrays) =>
- arrays.reduce((acc, array) => [...acc, ...array]);
+  arrays.reduce((acc, array) => [...acc, ...array]);
 console.log(
- `Result of concatMultiple: ${concatMultiple(
-   array1,
-   array2,
-   array3
- )} - original arrays: ${array1} -- ${array2} -- ${array3}`
+  `Result of concatMultiple: ${concatMultiple(
+    array1,
+    array2,
+    array3
+  )} - original arrays: ${array1} -- ${array2} -- ${array3}`
 );
 
 // 3. Clone Merge
@@ -64,54 +64,54 @@ console.log(`merged object: ${JSON.stringify(merge(object, object2))}`);
 
 // 4. Read books
 
-interface Book  {
- title: string;
- isRead: boolean;
-};
+interface Book {
+  title: string;
+  isRead: boolean;
+}
 const isBookRead = (books: Book[], titleToSearch: string): boolean => {
- const book = books.find(book => book.title === titleToSearch);
- return Boolean(book) ? book.isRead : false;
+  const book = books.find(book => book.title === titleToSearch);
+  return Boolean(book) ? book.isRead : false;
 };
 
 const books = [
- { title: "Harry Potter y la piedra filosofal", isRead: true },
- { title: "Canción de hielo y fuego", isRead: false },
- { title: "Devastación", isRead: true }
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true }
 ];
 
 console.log(`isRead 'Devastación': ${isBookRead(books, "Devastación")}`); // true
 console.log(
- `isRead 'Canción de hielo y fuego': ${isBookRead(
-   books,
-   "Canción de hielo y fuego"
- )}`
+  `isRead 'Canción de hielo y fuego': ${isBookRead(
+    books,
+    "Canción de hielo y fuego"
+  )}`
 ); // false
 console.log(
- `isRead 'Los Pilares de la Tierra': ${isBookRead(
-   books,
-   "Los Pilares de la Tierra"
- )}`
+  `isRead 'Los Pilares de la Tierra': ${isBookRead(
+    books,
+    "Los Pilares de la Tierra"
+  )}`
 ); // false
 
 // 5. Slot machine
 class SlotMachine {
- constructor() {
-   this.coins = 0;
- }
+  constructor() {
+    this.coins = 0;
+  }
 
- play() {
-   this.coins++;
-   if (this.spinWheel() && this.spinWheel() && this.spinWheel) {
-     console.log(`Congratulations!!!. You won ${this.coins} coins!!`);
-     this.coins = 0;
-   } else {
-     console.log("Good luck next time");
-   }
- }
+  play() {
+    this.coins++;
+    if (this.spinWheel() && this.spinWheel() && this.spinWheel()) {
+      console.log(`Congratulations!!!. You won ${this.coins} coins!!`);
+      this.coins = 0;
+    } else {
+      console.log("Good luck next time");
+    }
+  }
 
- spinWheel() {
-   return Math.random() >= 0.5;
- }
+  spinWheel() {
+    return Math.random() >= 0.5;
+  }
 }
 
 const machine1 = new SlotMachine();
@@ -127,4 +127,3 @@ machine1.play();
 machine1.play();
 machine1.play();
 machine1.play();
-
